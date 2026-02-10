c13cb5158a516423140d943b9a5f5846dc2092e6
surugurinethra29 <surugurinethra@gmail.com>
2026-02-10 19:59:35 +0530

    🚀 Day 13 - index.html - 7:59:29 PM

---
+++ Day-13/script.js
@@ -0,0 +1,7 @@
+// Day 13 Logic Practice
+console.log('Lab Session 13 Started');
+
+const display = document.getElementById('display');
+display.innerText = 'Logic Engine Online';
+
+// Practice your JS code here...
