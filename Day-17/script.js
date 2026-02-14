6bd1c06cd18b042960afebc119984190b86a1c50
surugurinethra29 <surugurinethra@gmail.com>
2026-02-14 19:24:16 +0530

    🚀 Day 17 - index.html - 7:24:14 PM

---
+++ Day-17/script.js
@@ -0,0 +1,7 @@
+// Day 17 Logic Practice
+console.log('Lab Session 17 Started');
+
+const display = document.getElementById('display');
+display.innerText = 'Logic Engine Online';
+
+// Practice your JS code here...
