68a288d3e693ab609a738ba3b68b77b5c6e51573
surugurinethra29 <surugurinethra@gmail.com>
2026-02-15 19:37:10 +0530

    🚀 Day 20 - index.html - 7:37:07 PM

---
+++ Day-20/script.js
@@ -0,0 +1,7 @@
+// Day 20 Logic Practice
+console.log('Lab Session 20 Started');
+
+const display = document.getElementById('display');
+display.innerText = 'Logic Engine Online';
+
+// Practice your JS code here...
